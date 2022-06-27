bb9b3af8df2ea6e3f7b87e5e75e0ac66655109a3
叶继伟 <729721503@qq.com>
2022-06-28 01:21:43 +0800

    add button component, part 1

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import Button, {ButtonType, ButtonSize} from './components/Button/button';
 function App() {
   return (
     <div className="App">
-      <Button>Hello</Button>
-      <Button btnType={ButtonType.Primary}>Hello</Button>
-      <Button btnType={ButtonType.Default} size={ButtonSize.Large}>Hello</Button>
-      <Button btnType={ButtonType.Link} href="https://www.baidu.com">Hello</Button>
+      <Button> Hello </Button>
+      <Button disabled> Disabled Button </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com"> Baidu Link </Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
     </div>
   );
 }
