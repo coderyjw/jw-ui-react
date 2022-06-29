3e0f70b07a0436147f6d05e4ea03a308ea225fa9
叶继伟 <729721503@qq.com>
2022-06-30 00:50:21 +0800

    add alert component part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
 import Button, {ButtonType, ButtonSize} from './components/Button/button';
+import Alert, { type } from './components/Alert/alert'
 
 function App() {
   return (
     <div className="App">
-      <Button autoFocus> Hello </Button>
-      <Button disabled> Disabled Button </Button>
-      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
-      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
-      <Button btnType={ButtonType.Link} href="http://www.baidu.com"> Baidu Link </Button>
-      <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
+      <div>
+        <Button autoFocus> Hello </Button>
+        <Button disabled> Disabled Button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com"> Baidu Link </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
+      </div>
+      <div>
+        <Alert title="this is a alert" type={type.Default}/>
+        <Alert title="this is a alert" type={type.Warning}/>
+        <Alert title="this is a alert" type={type.Success}/>
+        <Alert title="this is a alert" type={type.Danger}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Alert/alert.tsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import classNames from 'classnames'
+
+export enum type {
+  Default = 'default',
+  Success = 'success',
+  Danger = 'danger',
+  Warning = 'warning'
+}
+export interface AlertProps {
+  title: React.ReactNode,
+  description?: React.ReactNode,
+  type?: type,
+  onClose?: () => void;
+  closable?: boolean
+}
+const Alert: React.FC<AlertProps> = (props) => {
+  const [ hide, setHide ] = useState(false)
+
+  const {
+    title,
+    type,
+    description,
+    closable,
+    onClose
+  } = props
+  const classes = classNames('jw-alert', {
+    [`jw-alert-${type}`]: type,
+  })
+ 
+  const handleClose = (e: React.MouseEvent) => {
+    console.log(hide)
+    if (onClose) {
+      onClose()
+    }
+    setHide(true)
+  }
+
+  if(!hide) {
+    return (
+      <div className={classes}>
+        <span className="bold-title">{title}</span>
+        {description &&  <p className="jw-alert-desc">{description}</p>}
+        {closable && <span className="jw-alert-close" onClick={handleClose}>关闭</span>}
+      </div>
+    )
+  } else {
+    return <></>
+  }
+ 
+}
+
+Alert.defaultProps = {
+  type: type.Default,
+  closable: true,
+}
+export default Alert
