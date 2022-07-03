5678c802a86e0262fc7edfcc959f5a358b5ce5b3
叶继伟 <729721503@qq.com>
2022-07-04 02:10:31 +0800

    添加SubMenu组件测试

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,6 +1,7 @@
-import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react'
+import { cleanup, fireEvent, render, RenderResult,waitFor } from '@testing-library/react'
 import Menu, {MenuProps} from './menu'
 import MenuItem from './menuItem'
+import SubMenu from './subMenu'
 const testProps: MenuProps = {
   defaultIndex: '0',
   onSelect: jest.fn(),
@@ -18,14 +19,32 @@ const generateMenu = (props:MenuProps) => {
       <MenuItem >active</MenuItem>
       <MenuItem disabled>disabled</MenuItem>
       <MenuItem >deafult</MenuItem>
+      <SubMenu title="dropdown" >
+        <MenuItem>dropdown1</MenuItem>
+        <MenuItem>dropdown2</MenuItem>
+        <MenuItem>dropdown3</MenuItem>
+      </SubMenu>
     </Menu>
   )
 }
 
+const createStyleFile = () => {
+  const cssFile: string = `.jw-submenu {
+    display: none;
+  }
+    .jw-submenu.menu-opened {
+      display: block;
+    }
+  `
+  const style = document.createElement('style')
+  style.innerHTML = cssFile
+  return style
+}
 let wrapper:RenderResult,menuElement:HTMLElement,activeElement:HTMLElement,disabledElement:HTMLElement
 describe('测试Menu MenuItem组件', () => {
   beforeEach(() => {  
     wrapper = render(generateMenu(testProps))
+    wrapper.container.append(createStyleFile())
     menuElement = wrapper.getByTestId('test-menu')
     activeElement = wrapper.getByText('active')
     disabledElement = wrapper.getByText('disabled')
@@ -33,7 +52,7 @@ describe('测试Menu MenuItem组件', () => {
   it('是否渲染正确的Menu和MenuItem 基于默认的属性',() => {
     expect(menuElement).toBeInTheDocument()
     expect(menuElement).toHaveClass('jw-menu test')
-    expect(menuElement.getElementsByTagName('li').length).toEqual(3)
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4)
     expect(activeElement).toHaveClass('jw-menu-item is-active')
     expect(disabledElement).toHaveClass('jw-menu-item is-disabled')
   })
@@ -43,10 +62,10 @@ describe('测试Menu MenuItem组件', () => {
     fireEvent.click(thirditem)
     expect(thirditem).toHaveClass('jw-menu-item is-active')
     expect(activeElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).toHaveBeenCalledWith(2)
+    expect(testProps.onSelect).toHaveBeenCalledWith('2')
     fireEvent.click(disabledElement)
     expect(disabledElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+    expect(testProps.onSelect).not.toHaveBeenCalledWith('1')
   })
 
   it('mode为vertical时，是否正确渲染',() =>{
@@ -54,5 +73,21 @@ describe('测试Menu MenuItem组件', () => {
     const wrapper = render(generateMenu(testVerProps))
     menuElement = wrapper.getByTestId('test-menu')
     expect(menuElement).toHaveClass('menu-vertical')
+  })
+
+  it('测试下拉菜单', async () => {
+    expect(wrapper.queryByText('dropdown1')).not.toBeVisible()
+    const dropdownElement = wrapper.getByText('dropdown')
+    fireEvent.mouseEnter(dropdownElement)
+    await waitFor(() => {
+      expect(wrapper.queryByText('dropdown1')).toBeVisible()
+    })
+    fireEvent.click(wrapper.getByText('dropdown1'))
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0')
+    fireEvent.mouseLeave(dropdownElement)
+    await waitFor(() => {
+      expect(wrapper.queryByText('dropdown1')).toBeVisible()
+    })
+
   })
 })
