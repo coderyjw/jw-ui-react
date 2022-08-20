1b886fd528cafe5a93464d1cdbe4447841b8408c
叶继伟 <729721503@qq.com>
2022-08-20 14:23:35 +0800

    upload组件 part2 添加onProgress beforeUplaod周期函数

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -3,11 +3,20 @@ import { storiesOf} from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { Upload } from './upload'
 
-
+const log = (e: string)=> {
+  console.log(e)
+  return true
+}
 const SimpleUpload = () => {
   return (
-    <Upload action="https://jsonplaceholder.typicode.com/posts"></Upload>
-  )
+    <Upload action="https://jsonplaceholder.typicode.com/posts"
+      onProgress={action('progress')}
+      onSuccess={action('success')}
+      onError={action('error')}
+      onChange={action('change')}
+      beforeUpload={e => log('beforeUpload')}
+      />
+    )
 }
 
 
--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -8,10 +8,12 @@ export interface UploadProps  {
   onProgress?: (percent: number, file: File) => void;
   onSuccess?: (data: any, file: File) => void; 
   onError?: (err: any, file: File) => void;
+  beforeUpload?: (file:File) => boolean | Promise<File>;
+  onChange?: (file:File) => void
 }
 
 export const Upload: FC<UploadProps> = props => {
-  const { action,onError,onProgress,onSuccess } = props
+  const { action, onError, onProgress, onSuccess, beforeUpload, onChange } = props
   const fileInput = useRef<HTMLInputElement>(null)
   const handleClick = () => {
     if(fileInput.current) {
@@ -31,32 +33,49 @@ export const Upload: FC<UploadProps> = props => {
   const uploadFiles = (files: FileList) => {
     let postFiles = Array.from(files)
     postFiles.forEach(file => {
-      const formData = new FormData()
-      formData.append(file.name, file)
-      axios.post(action,formData,{
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        },
-        onUploadProgress: (e) => {
-          let percentage = Math.round((e.loaded * 100) / e.total) || 0;
-            console.log(percentage, file)
-            if (percentage < 100) {
-            if(onProgress) {
-              onProgress(percentage, file)
-            }
+      if(!beforeUpload) {
+        post(file)
+      } else {
+        const result = beforeUpload(file)
+        if(result && result instanceof Promise) {
+          result.then(processedFile => {
+            post(processedFile)
+          })
+        } else if(result !== false) {
+          post(file)
+        }
+      }
+    })
+  }
+
+  const post = (file:File) => {
+    const formData = new FormData()
+    formData.append(file.name, file)
+    axios.post(action,formData,{
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      onUploadProgress: (e) => {
+        let percentage = Math.round((e.loaded * 100) / e.total) || 0;
+        console.log(percentage, file)
+        if (percentage < 100) {
+          if(onProgress) {
+            onProgress(percentage, file)
           }
         }
-      }).then(res => {
-        console.log({res})
-        if(onSuccess) {
-          onSuccess(res.data,file)
-        }
-      }).catch(err => {
-        console.log(err)
-        if(onError) {
-          onError(err, file)
-        }
-      })
+      }
+    }).then(res => {
+      console.log({res})
+      if(onSuccess) {
+        onSuccess(res.data,file)
+      }
+      if(onChange) onChange(file)
+    }).catch(err => {
+      console.log(err)
+      if(onError) {
+        onError(err, file)
+      }
+      if(onChange) onChange(file)
     })
   }
   return (
